3ad2966a2fc61b2b7add44b71830fd302d7937e6
ekianjo <ekianjo@gmail.com>
2013-07-08 16:20:11 +0900

    Create main.py
    Outline of the project added.

---
+++ main.py
@@ -0,0 +1,8 @@
+#Start with simple application
+#title page with logo or simply text of petrolicious with cool font
+#controls are simple and intuitive
+#use ytdl for downloading videos
+#use mplayer for video playback
+#detects the best video format/size for the system
+#sync with online server for pictures - and videos. 
+#transition effects between scenes
