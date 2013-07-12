f799e832ddc4c66b5c42f31f8bd3e90fc6672ba5
ekianjo <ekianjo@gmail.com>
2013-07-12 11:59:05 +0900

    Update main.py
    updated outline of the project

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 #Start with simple application
 #title page with logo or simply text of petrolicious with cool font
-#controls are simple and intuitive
-#use ytdl for downloading videos
-#use mplayer for video playback
-#detects the best video format/size for the system
-#sync with online server for pictures - and videos. 
-#transition effects between scenes
+#use pygame or mplayer to play music in menu
+#controls are simple and intuitive - keyboard based
+#use ytdl for downloading videos - display download status or bar
+#use mplayer for video playback - once video downloaded. 
+#detects the best video format/size for the system - for pandora it would be 480 webm. for desktop probably Full HD
+#sync with online server for pictures - and videos list. 
+#transition effects between scenes (fade in fade out, with slight movement).
+
+#confirm performance of pygame libraries for this. Should be good enough, but need to check.
