2c0c35a5908efb0709a50c26cc86b88305474775
ekianjo <ekianjo@gmail.com>
2013-08-02 11:40:06 +0900

    Update main.py
    more details in program structure added.

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
 #Start with simple application
+
+#get logo online. Do not distribute it.
 #title page with logo or simply text of petrolicious with cool font
-#use pygame or mplayer to play music in menu
+
+#use pygame or mplayer to play music in menu - need to choose music.
+
+#0. Please wait message : caching all info retrieved online. Can take a while - play music in parallel
+#1. Logo
+#2. fade in tagline below Logo
+#3. fade out and fade on interface - it displays the liat of latest videos, along with small screenshot. 'Play' button appears on the right of videos already downloaded
+#4. use D-pad to move up or down in the list.
+#5. click on button -> fade out
+#6. Fade in video page -> description and download button.
+#7. Click on download -> launches download. 
+#8. Bar fills up
+#9. When downloaded, two options: erase or play back
+#10. Playback stops menu music. 
+#11. then starts video in full screen
+#display list of latest videos. 
+
 #controls are simple and intuitive - keyboard based
 #use ytdl for downloading videos - display download status or bar
 #use mplayer for video playback - once video downloaded. 
